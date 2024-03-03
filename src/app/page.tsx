efb5bc198ab702293d9e77400bc5893db3ddf0f4
shahbaz48 <shahbaz@capabl.com>
2024-03-03 10:52:20 +0000

    hide map component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Mapbox from "@/components/Map/Mapbox";
 import Todo from "@/components/Todo/Index";
 
 async function Index() {
   return (
     <>
       <main className="p-5 w-[100%] h-screen">
-        {/* <Mapbox /> */}
         <Todo />
       </main>
     </>
